3d1cd4949602216cb61717eb26fba85e2cbdf559
Mike Wyatt <wyatt.mike@gmail.com>
2015-10-27 23:37:48 -0300

    update react

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,10 +4,11 @@ import template from './view'
 $(document.getElementsByClassName('jade-example')[0]).html(template())
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './components/App'
 //window.React = React
 
-React.render(
+ReactDOM.render(
   <App />,
   document.getElementsByClassName('cosmos')[0]
 )
